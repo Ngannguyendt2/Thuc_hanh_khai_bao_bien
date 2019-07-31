8fabae8ec3593e867cc06bfc9893ac9832ea0571
= <=>
2019-07-31 09:01:14 +0700

    up file_thuc hanh khai bao bien

---
+++ kiem_tra_boi_so.js
@@ -0,0 +1,14 @@
+
+function test() {
+    let a;
+    let b;
+    let c;
+    a=prompt("Nhap so thu nhat ", '');
+    b=prompt("Nhap so thu 2 ", '');
+    if (a%b==0)
+    {
+        alert('b la boi so cua a')
+    }
+    else
+        alert('b khong la boi so cua a');
+}
